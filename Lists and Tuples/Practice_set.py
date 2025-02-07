efe732f99ecf9db6e8606379f4488fd6f48b9c71
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2025-02-07 20:49:08 +0530

    Lists and Tuples

---
+++ Lists and Tuples/Practice_set.py
@@ -0,0 +1,53 @@
+#                                   PRACTICE SET
+
+# 1. Write a program to store seven fruits in a list entered by the user.
+# fruits =[]
+# f1 = input('Enter fruit1')
+# fruits.append(f1)
+# f2 = input('Enter fruit2')
+# fruits.append(f2)
+# f3 = input('Enter fruit3')
+# fruits.append(f3)
+# f4 = input('Enter fruit4')
+# fruits.append(f4)
+# f5 = input('Enter fruit5')
+# fruits.append(f5)
+# f6 = input('Enter fruit6')
+# fruits.append(f6)
+# f7 = input('Enter fruit7')
+# fruits.append(f7)
+
+# print('Fruits:', fruits)
+
+
+
+# 2. Write a program to accept marks of 6 students and display them in a sorted  manner. 
+# marks =[]
+# student1= int(input('Enter marks for student 1'))
+# marks.append(student1)
+# student2= int(input('Enter marks for student 2'))
+# marks.append(student2)
+# student3= int(input('Enter marks for student 3'))
+# marks.append(student3)
+# student4= int(input('Enter marks for student 4'))
+# marks.append(student4)
+# student5= int(input('Enter marks for student 5'))
+# marks.append(student5)
+# student6= int(input('Enter marks for student 6'))
+# marks.append(student6)
+# marks.sort()
+# print(f"Marks of students in order {marks}")
+
+# 3. Check that a tuple type cannot be changed in python. 
+# a =[1,3.14,"apple"]
+# a[2]="big apple"
+# print(a)
+
+# 4. Write a program to sum a list with 4 numbers. 
+# l=[12,23,34,45,]
+# print(sum(l))
+
+# 5. Write a program to count the number of zeros in the following tuple: 
+# a = (7, 0, 8, 0, 0, 9) 
+# n=a.count(0)
+# print(n)
